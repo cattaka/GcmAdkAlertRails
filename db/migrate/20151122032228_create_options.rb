222b124067c9b6d940b9a99610260b59ff060398
cattaka <cattaka@mail.cattaka.net>
2015-11-22 16:28:06 +0900

    Fix wrong column name

--- db/migrate/20151122032228_create_options.rb
+++ db/migrate/20151122032228_create_options.rb
@@ -1,7 +1,7 @@
 class CreateOptions < ActiveRecord::Migration
   def change
     create_table :options do |t|
-      change_column :device_groups, :user_id, :user, null: false
+      change_column :device_groups, :user_id, :integer, null: false
       change_column :devices, :device_group_id, :device_group, null: false
     end
   end
